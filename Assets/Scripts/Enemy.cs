9e25f3dabec3c363021bfbd3fba6ed21439d4d90
Christopher Thomas Suitor <christopher.suitor@uvm.edu>
2019-12-30 15:58:56 -0600

    added player death

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    Rigidbody2D enemyRigidbody;
+    [SerializeField] float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFacingRight())
+        {
+            enemyRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            enemyRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    bool isFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(enemyRigidbody.velocity.x)), 1f);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,14 @@ public class Player : MonoBehaviour
 
     [SerializeField] float moveSpeed = 1;
     [SerializeField] float jumpHeight = 1;
+    [SerializeField] Vector2 dyingVector = new Vector2(2f, 2f);
     Animator playerAnimator;
     Rigidbody2D playerRigidbody;
+    CapsuleCollider2D playerCollider;
     BoxCollider2D feet;
 
     bool canDoubleJump = true;
-
+    bool isAlive;
     int count = 0;
 
     // Start is called before the first frame update
@@ -20,13 +22,30 @@ public class Player : MonoBehaviour
     {
         playerAnimator = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
         feet = GetComponent<BoxCollider2D>();
+        isAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isAlive) { return; }
         movement();
+        death();
+    }
+
+    private void death()
+    {
+        if (playerCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        {
+            isAlive = false;
+        }
+        if (!isAlive)
+        {
+            playerAnimator.SetTrigger("isDying");
+            playerRigidbody.velocity = dyingVector;
+        }
     }
 
     // Move the player according to user input. Also, execute running animaiton.
