3eee2ff25a9a1966c3b0836260536926407efafc
Christopher Thomas Suitor <christopher.suitor@uvm.edu>
2020-01-02 17:40:52 -0600

    able to load next level upon level completion

---
+++ Assets/Scripts/ExitPortal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitPortal : MonoBehaviour
+{
+    [SerializeField] float loadTime = 3f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine("loadNextScene");
+    }
+
+    IEnumerator loadNextScene()
+    {
+        yield return new WaitForSecondsRealtime(loadTime);
+
+        var currScene = SceneManager.GetActiveScene();
+        int currBuildIndex = currScene.buildIndex;
+        SceneManager.LoadScene(currBuildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private void Awake()
+    {
+        // establish singleton
+        if(FindObjectsOfType<GameController>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
